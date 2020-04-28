99bdbe56a17b58ccee5efe4a4b3fc0e95430cbc0
Mees Fix <mfix@stsci.edu>
2020-04-28 19:20:41 -0400

    Adding nbpath to convert.py, updating jenkinsfile to reflect and pointing to upstream nbpages in requirements

--- convert.py
+++ convert.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
-
+import argparse
 import os
 import logging
 
 from nbpages import make_parser, run_parsed, make_html_index
 
-args = make_parser().parse_args()
+args = make_parser()
+args.add_argument("--notebook-path", default='.', dest='nb_path',
+                  help="A relative path to notebooks you wish to execute.")
+args = args.parse_args()
+
 if args.template_file is None and os.path.exists('nb_html.tpl'):
     args.template_file = 'nb_html.tpl'
 
-if args.exclude is None and args.include is None:
+if args.exclude is None:
     # If there is an "exclude_notebooks" file, use that to find which ones to
     # skip.  Format is one pattern per line, "#" for comments.
-    # Note that this will be ignored if exclude or include is given at the command line.
+    # note that this will be ignored if exclude is given at the command line.
     to_exclude = []
     if os.path.isfile('exclude_notebooks'):
         with open('exclude_notebooks') as f:
             for line in f:
                 if line.strip() != '':
-                    to_exclude.append(line.split('#')[0].strip())
-
+                    to_exclude.append(line.strip().split('#')[0])
     if to_exclude:
         args.exclude = ','.join(to_exclude)
 
-converted = run_parsed('jwst_validation_notebooks', output_type='HTML', args=args)
+converted = run_parsed(args.nb_path, output_type='HTML', args=args)
 
 logging.getLogger('nbpages').info('Generating index.html')
 make_html_index(converted, './index.tpl')
